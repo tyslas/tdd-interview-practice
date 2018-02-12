3d77a4f1f35bd879b6bc9e288cd26dc7f8597e7c
Tito Yslas <roberto.yslas@gmail.com>
2018-02-12 00:04:09 -0700

    the MovieStore is now able to return movies that contain the same characters as a users search - validated by a test

--- src/main/java/MovieStore.java
+++ src/main/java/MovieStore.java
@@ -3,17 +3,16 @@ import model.Movie;
 import java.util.LinkedList;
 import java.util.List;
 
-import static java.util.Arrays.asList;
-
 public class MovieStore {
   List<Movie> movies = new LinkedList<Movie>();
   public List<Movie> findByPartialTitle(String partialTitle) {
+    List<Movie> result = new LinkedList<Movie>();
     for (Movie movie : movies) {
-      if(movie.title().contains(partialTitle)) {
-        return asList(movie);
+      if(movie.title().toUpperCase().contains(partialTitle.toUpperCase())) {
+        result.add(movie);
       }
     }
-    return new LinkedList<Movie>();
+    return result;
   }
 
   public void add(Movie movie) {
